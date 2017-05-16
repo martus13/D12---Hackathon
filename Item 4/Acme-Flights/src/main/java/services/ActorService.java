87cc0877b8c929d02522459754064bd0c85a0691
pedrosr14 <p.serrano.r.5@gmail.com>
2017-05-16 05:11:31 +0200

    Repos y servicios de Admin y Actor

---
+++ Item 4/Acme-Flights/src/main/java/services/ActorService.java
@@ -0,0 +1,113 @@
+package services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import repositories.ActorRepository;
+import security.Authority;
+import security.LoginService;
+import security.UserAccount;
+import domain.Actor;
+
+@Service
+@Transactional
+public class ActorService {
+
+	// Managed repository -----------------------------------------------------
+	@Autowired
+	private ActorRepository	actorRepository;
+
+
+	// Supporting services ----------------------------------------------------
+
+	// Constructors -----------------------------------------------------------
+	public ActorService() {
+		super();
+	}
+
+	// Simple CRUD methods ----------------------------------------------------
+
+	public Actor findOne(final int actorId) {
+		Actor result;
+
+		result = this.actorRepository.findOne(actorId);
+
+		return result;
+	}
+
+	public Collection<Actor> findAll() {
+		Collection<Actor> result;
+
+		result = this.actorRepository.findAll();
+
+		return result;
+	}
+
+	public Actor save(Actor actor) {
+
+		if (actor.getId() == 0) {
+			String password;
+			String encryptedPassword;
+
+			password = actor.getUserAccount().getPassword();
+			encryptedPassword = this.encryptPassword(password);
+
+			actor.getUserAccount().setPassword(encryptedPassword);
+
+		}
+		actor = this.actorRepository.save(actor);
+
+		return actor;
+	}
+
+	// Other business methods -------------------------------------------------
+
+	public Actor findByPrincipal() {
+		Actor result;
+		UserAccount userAccount;
+
+		userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+		result = this.findByUserAccountId(userAccount.getId());
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	public Actor findByUserAccountId(final int userAccountId) {
+		Assert.notNull(userAccountId);
+
+		Actor result;
+
+		result = this.actorRepository.findByUserAccountId(userAccountId);
+
+		return result;
+	}
+
+	public Boolean checkAuthority(final Actor actor, final String authority) {
+		Boolean result;
+		Collection<Authority> authorities;
+
+		authorities = actor.getUserAccount().getAuthorities();
+
+		result = false;
+		for (final Authority a : authorities)
+			result = result || (a.getAuthority().equals(authority));
+
+		return result;
+	}
+
+	public String encryptPassword(String password) {
+		Md5PasswordEncoder encoder;
+
+		encoder = new Md5PasswordEncoder();
+		password = encoder.encodePassword(password, null);
+
+		return password;
+	}
+}
---
+++ Item 4/Acme-Flights/src/main/java/services/AdministratorService.java
@@ -0,0 +1,76 @@
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import repositories.AdministratorRepository;
+import security.LoginService;
+import security.UserAccount;
+import domain.Administrator;
+
+@Service
+@Transactional
+public class AdministratorService {
+
+	// Managed repository -----------------------------------------------------
+	@Autowired
+	private AdministratorRepository	administratorRepository;
+
+
+	// Supporting services ----------------------------------------------------
+
+	// Constructors -----------------------------------------------------------
+	public AdministratorService() {
+		super();
+	}
+
+	// Simple CRUD methods ----------------------------------------------------
+
+	public Administrator findOne(final int adminId) {
+		Assert.notNull(adminId);
+		Assert.isTrue(adminId != 0);
+
+		final Administrator result = this.administratorRepository.findOne(adminId);
+
+		return result;
+	}
+
+	public Collection<Administrator> findAll() {
+		Collection<Administrator> result;
+
+		result = this.administratorRepository.findAll();
+
+		return result;
+	}
+
+	// Other business methods -------------------------------------------------
+
+	public Administrator findByPrincipal() {
+		Administrator result;
+		UserAccount userAccount;
+
+		userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+		result = this.findByUserAccountId(userAccount.getId());
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	public Administrator findByUserAccountId(final int userAccountId) {
+		Assert.notNull(userAccountId);
+		Assert.isTrue(userAccountId != 0);
+
+		Administrator result;
+
+		result = this.administratorRepository.findByUserAccountId(userAccountId);
+
+		return result;
+	}
+
+}
