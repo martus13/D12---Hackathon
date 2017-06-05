994ef46594f89a56ced856a0265e269423fc55ec
Shelux8 <shelux1987@gmail.com>
2017-06-05 02:39:57 +0200

    OfferService y correción de FinderService (tests)

--- Item 4/Acme-Flights/src/test/java/services/FinderServiceTest.java
+++ Item 4/Acme-Flights/src/test/java/services/FinderServiceTest.java
@@ -29,21 +29,25 @@ public class FinderServiceTest extends AbstractTest {
 	@Autowired
 	private UserService		userService;
 
+	@Autowired
+	private AirportService	airportService;
+
 
 	// Tests ------------------------------------------------------------------
 
 	@Test
 	public void driverSave() {
+
 		final Object testingData[][] = {
 			{	// Bien
-				"user1", null, 2, 5, true
-			}, {//usuario incorrecto
-				"m", IllegalArgumentException.class, 2, 5, true
+				"user1", null, 2, 5, true, 112, 114
+			}, {//Error: mismo destino de llegada y salida
+				"user1", IllegalArgumentException.class, 2, 5, true, 112, 112
 			}
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.testEdit((String) testingData[i][0], (Class<?>) testingData[i][1], (Integer) testingData[i][2], (Integer) testingData[i][3], (Boolean) testingData[i][4]);
+			this.testEdit((String) testingData[i][0], (Class<?>) testingData[i][1], (Integer) testingData[i][2], (Integer) testingData[i][3], (Boolean) testingData[i][4], (Integer) testingData[i][5], (Integer) testingData[i][6]);
 	}
 	//Finds
 	@Test
@@ -60,7 +64,7 @@ public class FinderServiceTest extends AbstractTest {
 			this.testFindByUserId((String) testingData[i][0], (Class<?>) testingData[i][1]);
 	}
 
-	protected void testEdit(final String username, final Class<?> expected, final int childrenNumber, final int passengersNumber, final boolean isBusiness) {
+	protected void testEdit(final String username, final Class<?> expected, final int childrenNumber, final int passengersNumber, final boolean isBusiness, final int departId, final int destId) {
 		Class<?> caught;
 
 		caught = null;
@@ -72,6 +76,9 @@ public class FinderServiceTest extends AbstractTest {
 			finder.setChildrenNumber(childrenNumber);
 			finder.setPassengersNumber(passengersNumber);
 			finder.setIsBusiness(isBusiness);
+			finder.setDeparture(this.airportService.findOne(departId));
+			finder.setDestination(this.airportService.findOne(destId));
+			finder = this.finderService.save(finder);
 
 			Assert.notNull(finder);
 
--- Item 4/Acme-Flights/src/test/java/services/OfferServiceTest.java
+++ Item 4/Acme-Flights/src/test/java/services/OfferServiceTest.java
@@ -2,7 +2,9 @@
 package services;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 
 import javax.transaction.Transactional;
 
@@ -51,41 +53,32 @@ public class OfferServiceTest extends AbstractTest {
 			this.testCreate((String) testingData[i][0], (Class<?>) testingData[i][1], (Integer) testingData[i][2], (Integer) testingData[i][3]);
 	}
 
-	//	// Eliminación de una campaña:
-	//	@Test
-	//	public void driverDelete() {
-	//		final Object testingData[][] = {
-	//			{//Bien
-	//				"manager2", 151, null
-	//			}, {//no se puede borrar porque tiene facturas sin pagar
-	//				"manager1", 149, IllegalArgumentException.class
-	//			}
-	//		};
-	//
-	//		for (int i = 0; i < testingData.length; i++)
-	//			this.testDelete((String) testingData[i][0], (int) testingData[i][1], (Class<?>) testingData[i][2]);
-	//	}
-	//
-	//	//Finds
-	//	@Test
-	//	public void driverFind() {
-	//		final Object testingData[][] = {
-	//			{	// Bien
-	//				"manager1", null, 118
-	//			}, {//usuario mal
-	//				"m", IllegalArgumentException.class, 118
-	//			}, {
-	//				"manager2", null, 119
-	//			}
-	//		};
-	//
-	//		for (int i = 0; i < testingData.length; i++) {
-	//			this.testFindActiveByAirlineId((String) testingData[i][0], (Class<?>) testingData[i][1], (Integer) testingData[i][2]);
-	//			this.testFindByAirlineId((String) testingData[i][0], (Class<?>) testingData[i][1], (Integer) testingData[i][2]);
-	//			this.testFindNotDeletedByAirlineId((String) testingData[i][0], (Class<?>) testingData[i][1], (Integer) testingData[i][2]);
-	//		}
-	//	}
-	//
+	@Test
+	public void driverEdit() {
+		Calendar correctCalendar1;
+		Calendar correctCalendar2;
+		Calendar wrongCalendar;
+
+		correctCalendar1 = Calendar.getInstance();
+		correctCalendar1.set(2017, 5, 24, 23, 58, 00);
+		correctCalendar2 = Calendar.getInstance();
+		correctCalendar2.set(2017, 5, 24, 23, 59, 00);
+
+		wrongCalendar = Calendar.getInstance();
+		wrongCalendar.set(2017, 5, 17, 23, 58, 00);
+
+		final Object testingData[][] = {
+			{	// Bien
+				"manager1", null, 133, 134, correctCalendar1.getTime(), correctCalendar2.getTime()
+			}, {
+				"manager1", IllegalArgumentException.class, 133, 134, correctCalendar1.getTime(), wrongCalendar.getTime()
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testSave((String) testingData[i][0], (Class<?>) testingData[i][1], (Integer) testingData[i][2], (Integer) testingData[i][3], (Date) testingData[i][4], (Date) testingData[i][5]);
+	}
+
 	protected void testCreate(final String username, final Class<?> expected, final int offertable1, final int offertable2) {
 		Class<?> caught;
 
@@ -117,56 +110,38 @@ public class OfferServiceTest extends AbstractTest {
 
 	}
 
-	//	public Collection<Offer> findByAirlineId(final int airlineId) {
-	//		Collection<Offer> result;
-	//
-	//		result = this.offerRepository.findByAirlineId(airlineId);
-	//
-	//		return result;
-	//	}
-	//
-	//	public Offer findByDateAndOffertableId(final int offertableId, final Date date) {
-	//		Assert.isTrue(offertableId != 0);
-	//		Assert.notNull(date);
-	//
-	//		Offer result;
-	//
-	//		result = this.offerRepository.findByDateAndOffertableId(offertableId, date);
-	//
-	//		return result;
-	//	}
+	protected void testSave(final String username, final Class<?> expected, final int offertable1, final int offertable2, final Date startMoment, final Date endMoment) {
+		Class<?> caught;
 
-	//	public Collection<Offer> findOverlappingByOffer(final Offer offer) {
-	//		Assert.notNull(offer);
-	//
-	//		Collection<Offer> result;
-	//
-	//		result = this.offerRepository.findOverlappingByOffer(offer.getOffertables(), offer.getStartMoment(), offer.getEndMoment(), offer.getId());
-	//
-	//		return result;
-	//	}
-	//	public Collection<Offer> findByDateAndOffertables(final Collection<Offertable> offertables, final Date date) {
-	//		Assert.notNull(offertables);
-	//		Assert.notNull(date);
-	//
-	//		Collection<Offer> result;
-	//
-	//		result = new ArrayList<Offer>();
-	//
-	//		for (final Offertable o : offertables) {
-	//			Offer offer;
-	//
-	//			offer = this.offerRepository.findByDateAndOffertableId(o.getId(), date);
-	//
-	//			if (o instanceof Flight && offer == null)
-	//				offer = this.offerRepository.findByDateAndOffertableId(((Flight) o).getAirline().getId(), date);
-	//
-	//			if (offer != null)
-	//				result.add(offer);
-	//
-	//		}
-	//
-	//		return result;
-	//	}
+		caught = null;
+		try {
+			this.authenticate(username);
+			Offer offer;
+			Collection<Offertable> offertables;
+
+			offertables = new ArrayList<Offertable>();
+			offertables.add(this.offertableService.findOne(offertable1));
+			Assert.notNull(this.offertableService.findOne(offertable1));
+			offertables.add(this.offertableService.findOne(offertable2));
+			Assert.notNull(this.offertableService.findOne(offertable2));
+
+			offer = this.offerService.create();
+			offer.setOffertables(offertables);
+			offer.setEndMoment(endMoment);
+			offer.setStartMoment(startMoment);
+
+			offer = this.offerService.save(offer);
+
+			Assert.notNull(offer);
+
+			this.unauthenticate();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		this.checkExceptions(expected, caught);
+
+	}
 
 }
